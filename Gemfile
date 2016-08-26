d23e18b857d4a0d5873380a8d4f2d9af1288e335
schoetlr <srlrs20020@gmail.com>
2016-08-26 15:56:44 -0400

    updated seed file

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'hirb'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+gem 'faker'
+
 # Use Unicorn as the app server
 # gem 'unicorn'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,68 @@
 
 
 Comment.destroy_all
+User.destroy_all
+Post.destroy_all
+Like.destroy_all
+Profile.destroy_all
 
+puts "creating users"
+5.times do 
+  u = User.create(email: Faker::Internet.safe_email,
+              password: "password",
+              password_confirmation: "password")
+  
+  u.build_profile(phone: Faker::PhoneNumber.phone_number,
+                  birthday: Time.now,
+                  college: Faker::University.name,
+                  hometown: Faker::Address.city,
+                  current_location: Faker::Address.city,
+                  about: "This is an about me", 
+                  words_to_live_by: "These are words to live by",
+                  first_name: Faker::Name.name,
+                  last_name: Faker::Name.name,
+                  gender: ["male", "female"].sample
+                  )
+end
+
+def random_user_id
+  User.all.sample.id
+end
+puts "creating posts"
+50.times do 
+  Post.create(:user_id => random_user_id,
+              content: Faker::Lorem.paragraph)
+
+end
+
+def random_post_id
+  Post.all.sample.id
+end
+puts "creating comments"
+50.times do 
+  Comment.create(user_id: random_user_id,
+                post_id: random_post_id,
+                content: Faker::Lorem.paragraph)
+
+end
+
+def like_params
+  type = ["Post", "Comment"].sample
+  id = type.constantize.all.sample.id
+  user_id = 1
+  while Like.all.map {|like| like.user_id }.include?(user_id)
+    user_id = User.all.sample.id
+  end
+
+  {
+    likeable_type: type,
+    likeable_id: id,
+    user_id: 1,
+
+  }
+end
+
+puts 'creating likes'
+50.times do 
+  Like.create(like_params)
+end
