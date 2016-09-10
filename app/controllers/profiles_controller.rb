0c89dc24c12465182c4c7e3487d77ef9ac62053b
schoetlr <srlrs20020@gmail.com>
2016-09-10 10:19:58 -0400

    like photo comments

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,7 +5,9 @@ class ProfilesController < ApplicationController
   def set_profile_photo
         
     photo_id = params[:photo_id].to_i
-    unless photo_id == current_user.profile_photo.id
+    if photo_id == current_user.profile.profile_photo_id
+      flash[:error] = "Already set as cover photo"
+    else
       profile = current_user.profile
       
       profile.set_profile_photo(photo_id)
@@ -17,12 +19,15 @@ class ProfilesController < ApplicationController
   def set_cover_photo
     
     photo_id = params[:photo_id].to_i
-    unless photo_id == current_user.profile_photo.id
+    if photo_id == current_user.profile.cover_photo_id
+      flash[:error] = "Already set as cover photo"
+    else
       profile = current_user.profile
 
       profile.set_cover_photo(photo_id)
       flash[:success] = "Cover photo set"
     end
+    
     redirect_to :back
   end
 end
--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -11,4 +11,15 @@ module PhotosHelper
     str.html_safe
   end
 
+  def like_link_display_comment(comment)
+    
+    if comment.liked_by_user?(current_user)
+      like = Like.where(user_id: current_user.id).first
+      str = link_to("Unlike", like_path(like), method: "DELETE")
+    else 
+      str = link_to("Like", comment_likes_path(comment.id), method: "POST")
+    end
+    str.html_safe
+  end
+
 end
